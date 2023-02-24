4b9abe1043221c608377b8e4aedb462eb5352809
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-02-23 23:25:42 -0300

    feat: create Character class

---
+++ src/Character.ts
@@ -0,0 +1,84 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(private _name: string) {
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage(this._name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = { 
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  attack(enemy: Fighter): void {
+    throw new Error('Method not implemented.');
+  }
+
+  levelUp(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this._defense - attackPoints;
+
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+
+    if (damage <= 0) {
+      this._lifePoints -= 1;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  get defense() {
+    return this._defense;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  get energy() {
+    return this._energy;
+  }
+}
